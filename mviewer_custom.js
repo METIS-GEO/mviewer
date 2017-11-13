b9c404f9eb6f7fad0e622fd2c1f4d56dcaaeda47
unknown <g.bruel@metis-reseaux.fr>
2017-11-13 17:19:32 +0100

    new init with clean files

---
+++ mviewer_custom.js
@@ -0,0 +1,14 @@
+/*
+ * 
+ * This file is part of mviewer
+ *
+ * here we write new custom functions
+ *
+ * mviewer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with mviewer.  If not, see <http://www.gnu.org/licenses/>.
+ */
